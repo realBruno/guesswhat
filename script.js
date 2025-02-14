bf49b8b5637db42ca13ce1b27821bdc1bf915654
realBruno <soubruno@protonmail.com>
2025-02-14 09:10:51 -0300

    Add generator

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-/* *************** TITLE *************** */
+function generate_password()
+{   
+    const length = document.getElementById("range_input").value;
+    let password = '';
+
+    for (let i = 0; i < length; i++)
+    {
+        let char = parseInt(Math.random() * 94 + 32) // generates a random number between 0 and 126;
+        password += String.fromCharCode(char);
+    }
+    const show_password = document.getElementById("show_password");
+    show_password.textContent = password;
+}
